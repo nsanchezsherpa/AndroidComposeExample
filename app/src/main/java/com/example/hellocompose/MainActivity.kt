21423a71cc82131f42c927a2e9dd9cf19e31e1e5
Norman Sánchez <nsanchez@sherpabrokers.com>
2022-09-06 23:41:50 -0500

    Adding new components

--- app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hellocompose.ui.theme.HelloComposeTheme
 
@@ -24,7 +28,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HelloCompose() {
-    Text(text = "Hello Compose :)")
+    Column {
+        Image(
+            painter = painterResource(
+                id = R.drawable.example_image),
+            contentDescription = null
+        )
+        Text("Curso de Jetpack Compose")
+        Text(text = "Norman Sanchez")
+        Button(onClick = {
+
+        }) {
+            Text(text = "Dale click")
+        }
+    }
 }
 
 @Preview(
