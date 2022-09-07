1ad5d6936d0a7e28de811a7a54c24d00c538e3c8
Norman Sánchez <nsanchez@sherpabrokers.com>
2022-09-07 11:07:53 -0500

    Modifiers

--- app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -2,20 +2,24 @@ package com.example.hellocompose
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.print.PrintAttributes
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.hellocompose.ui.theme.HelloComposeTheme
+import java.nio.file.Files.size
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,18 +32,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HelloCompose() {
-    Column {
-        Image(
-            painter = painterResource(
-                id = R.drawable.example_image),
-            contentDescription = null
-        )
-        Text("Curso de Jetpack Compose")
-        Text(text = "Norman Sanchez")
-        Button(onClick = {
+    Card (
+        elevation = 4.dp,
+        shape = RoundedCornerShape(20.dp),
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Column (
+            modifier = Modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.example_image),
+                contentDescription = null,
+                modifier = Modifier.size(width = 150.dp, height = 40.dp)
+            )
+            Text(
+                text = "Curso de Jetpack Compose",
+                style = MaterialTheme.typography.h4
+            )
+            Text(text = "Norman Sanchez")
+            Button(onClick = {
 
-        }) {
-            Text(text = "Dale click")
+            },
+                modifier = Modifier.padding(vertical = 16.dp)) {
+                Text(text = "Dale click")
+            }
         }
     }
 }
